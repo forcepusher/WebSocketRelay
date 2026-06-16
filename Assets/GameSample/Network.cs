7b9e5770dda328692a378123eedaddbc92943033
forcepusher <bionitsoup@gmail.com>
2026-06-17 00:23:14 +0700

    Add network

--- Assets/GameSample/Network.cs
+++ Assets/GameSample/Network.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using BananaParty.WebSocketRelay;
 
 namespace BananaParty.WebSocketRelay.Samples
 {
     public class Network : MonoBehaviour
     {
+        [SerializeField] private string serverAddress = "ws://127.0.0.1:23144";
+        private Socket _socket;
+
         public void StartServer()
         {
 
@@ -11,7 +15,14 @@ namespace BananaParty.WebSocketRelay.Samples
 
         public void StartClient()
         {
+            _socket = new Socket(serverAddress);
+            _socket.Connect();
+            Debug.Log($"Connected to relay server at {serverAddress}");
+        }
 
+        private void OnDestroy()
+        {
+            _socket?.Dispose();
         }
     }
 }
