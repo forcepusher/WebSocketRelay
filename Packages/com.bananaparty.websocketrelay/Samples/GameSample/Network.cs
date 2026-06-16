0d12a606ae33a7179f75286a198a75df3c230bd2
forcepusher <bionitsoup@gmail.com>
2026-06-17 01:38:59 +0700

    Add stop client

--- Packages/com.bananaparty.websocketrelay/Samples/GameSample/Network.cs
+++ Packages/com.bananaparty.websocketrelay/Samples/GameSample/Network.cs
@@ -7,13 +7,21 @@ namespace BananaParty.WebSocketRelay.Samples
     public class Network : MonoBehaviour
     {
         [SerializeField] private string serverAddress = "ws://127.0.0.1:23144";
+        private Server _server;
         private Socket _socket;
 
         public void StartServer()
         {
-            var server = new Server();
-            server.Start();
+            _server = new Server();
+            _server.Start();
             Debug.Log("Relay server started.");
+        }
+
+        public void StopServer()
+        {
+            _server?.Stop();
+            _server = null;
+            Debug.Log("Relay server stopped.");
         }
 
         public void StartClient()
@@ -23,8 +31,16 @@ namespace BananaParty.WebSocketRelay.Samples
             Debug.Log($"Connected to relay server at {serverAddress}");
         }
 
+        public void StopClient()
+        {
+            _socket?.Dispose();
+            _socket = null;
+            Debug.Log("Disconnected from relay server.");
+        }
+
         private void OnDestroy()
         {
+            _server?.Stop();
             _socket?.Dispose();
         }
     }
