29c66e472e7676eea70862427c4a1ddeeb460eae
forcepusher <bionitsoup@gmail.com>
2026-07-01 22:07:18 +0700

    Remove networidentity code

--- Packages/com.bananaparty.websocketrelay/Runtime/Networking/INetworkIdentityFactory.cs
+++
@@ -1,8 +0,0 @@
-namespace BananaParty.WebSocketRelay
-{
-    public interface INetworkIdentityFactory
-    {
-        NetworkIdentity Instantiate(string resourcePath, string topic);
-        void Destroy(NetworkIdentity networkIdentity);
-    }
-}
