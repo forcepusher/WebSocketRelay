c152486f0abe3134446cae98587caa48e1643912
forcepusher <bionitsoup@gmail.com>
2026-07-14 17:06:57 +0700

    Add channel

---
+++ Packages/com.bananaparty.websocketrelay/Runtime/Networking/Channel.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BananaParty.WebSocketRelay
+{
+    [CreateAssetMenu]
+    public class Channel : ScriptableObject
+    {
+        public List<ChannelBinding> _channelBindings;
+
+        public void AddBinding(ChannelBinding channel)
+        {
+            _channelBindings.Add(channel);
+        }
+
+        public void RemoveBinding(ChannelBinding channel)
+        {
+            _channelBindings.Remove(channel);
+        }
+
+        public void SetChannel(string channel)
+        {
+            foreach (ChannelBinding binding in _channelBindings)
+                binding.SetChannel(channel);
+        }
+    }
+}
---
+++ Packages/com.bananaparty.websocketrelay/Runtime/Networking/ChannelBinding.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace BananaParty.WebSocketRelay
+{
+    public class ChannelBinding : MonoBehaviour
+    {
+        [SerializeField]
+        private Channel _networkChannel;
+
+        private NetworkIdentity _networkIdentity;
+
+        private void Awake()
+        {
+            _networkIdentity = GetComponent<NetworkIdentity>();
+        }
+
+        private void OnEnable()
+        {
+            _networkChannel.AddBinding(this);
+        }
+
+        private void OnDisable()
+        {
+            _networkChannel.RemoveBinding(this);
+        }
+
+        public void SetChannel(string channel)
+        {
+            _networkIdentity.Channel = channel;
+        }
+    }
+}
