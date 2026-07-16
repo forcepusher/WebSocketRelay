79bbfd955619c086c8b2f5ef1bde84db5165e283
forcepusher <bionitsoup@gmail.com>
2026-07-17 02:32:18 +0700

    Add sample color switch

--- Packages/com.bananaparty.websocketrelay/Samples/GameSample/Character.cs
+++ Packages/com.bananaparty.websocketrelay/Samples/GameSample/Character.cs
@@ -3,19 +3,9 @@ using UnityEngine;
 namespace BananaParty.WebSocketRelay.Samples
 {
     [RequireComponent(typeof(CharacterController))]
-    public class Character : MonoBehaviour, INetworkState, IRpcTarget
+    public class Character : MonoBehaviour, INetworkState
     {
-        private const string RandomColorParametername = "RandomColor";
-
-        private enum RpcType
-        {
-            RandomColorOnLeftClick,
-            GreyColorOnRightClick
-        }
-
         public string NetworkStateName => nameof(Character);
-
-        public string RpcSubjectName => nameof(Character);
 
         public INetworkIdentity NetworkIdentity => _networkIdentity;
 
@@ -46,14 +36,6 @@ namespace BananaParty.WebSocketRelay.Samples
             _characterInput.PollInput();
 
             Move();
-
-            if (Input.GetMouseButton(0))
-            {
-                IStateOutput parametersOutput = _networkIdentity.NetworkContext.StateFormat.CreateOutput();
-                parametersOutput.WriteInt(nameof(RpcType), (int)RpcType.RandomColorOnLeftClick);
-                parametersOutput.WriteVector3(RandomColorParametername, new Vector3(Random.value, Random.value, Random.value));
-                _networkIdentity.SendRpc(RpcSubjectName, parametersOutput);
-            }
         }
 
         public void WriteNetworkState(IStateOutput stateOutput)
@@ -111,7 +93,5 @@ namespace BananaParty.WebSocketRelay.Samples
                 _characteController.Move(Vector3.up * _verticalVelocity * Time.deltaTime);
             }
         }
-
-        public void ReceiveRpc(IStateInput parametersStateInput) => throw new System.NotImplementedException();
     }
 }
---
+++ Packages/com.bananaparty.websocketrelay/Samples/GameSample/ColorSwitch.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+namespace BananaParty.WebSocketRelay.Samples
+{
+    public class ColorSwitch : MonoBehaviour, IRpcTarget
+    {
+        private const string RandomColorParametername = "RandomColor";
+
+        private NetworkIdentity _networkIdentity;
+
+        public INetworkIdentity NetworkIdentity => _networkIdentity;
+
+        private enum RpcType
+        {
+            RandomColorOnLeftClick,
+            GreyColorOnRightClick
+        }
+
+        public string RpcSubjectName => nameof(ColorSwitch);
+
+        private void Awake()
+        {
+            _networkIdentity = GetComponent<NetworkIdentity>();
+        }
+
+        private void Update()
+        {
+            if (_networkIdentity.NetworkAuthority)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    IStateOutput parametersOutput = _networkIdentity.NetworkContext.StateFormat.CreateOutput();
+                    parametersOutput.WriteInt(nameof(RpcType), (int)RpcType.RandomColorOnLeftClick);
+                    Vector3 color = new Vector3(Random.value, Random.value, Random.value);
+                    parametersOutput.WriteVector3(RandomColorParametername, color);
+                    _networkIdentity.SendRpc(RpcSubjectName, parametersOutput);
+
+                    SetColor(color);
+                }
+
+                if (Input.GetMouseButtonDown(1))
+                {
+                    IStateOutput parametersOutput = _networkIdentity.NetworkContext.StateFormat.CreateOutput();
+                    parametersOutput.WriteInt(nameof(RpcType), (int)RpcType.GreyColorOnRightClick);
+                    _networkIdentity.SendRpc(RpcSubjectName, parametersOutput);
+
+                    SetColor(new Vector3(0.5f, 0.5f, 0.5f));
+                }
+            }
+        }
+
+        private void SetColor(Vector3 color)
+        {
+            GetComponent<Renderer>().material.color = new Color(color.x, color.y, color.z);
+        }
+
+        public void ReceiveRpc(IStateInput parametersStateInput)
+        {
+            RpcType rpcType = (RpcType)parametersStateInput.ReadInt(nameof(RpcType));
+            switch (rpcType)
+            {
+                case RpcType.RandomColorOnLeftClick:
+                    Vector3 color = parametersStateInput.ReadVector3(RandomColorParametername);
+                    SetColor(color);
+                    break;
+                case RpcType.GreyColorOnRightClick:
+                    SetColor(new Vector3(0.5f, 0.5f, 0.5f));
+                    break;
+            }
+        }
+    }
+}
