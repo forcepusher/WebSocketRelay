ee26d2c5e5066fe3601724f3c240d8536ac8d022
forcepusher <bionitsoup@gmail.com>
2026-07-25 01:36:22 +0700

    Add sync for click objective

--- Packages/com.bananaparty.websocketrelay/Samples/GameSample/ClickObjective.cs
+++ Packages/com.bananaparty.websocketrelay/Samples/GameSample/ClickObjective.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 namespace BananaParty.WebSocketRelay.Samples
 {
-    public class ClickObjective : MonoBehaviour
+    public class ClickObjective : MonoBehaviour, INetworkState
     {
+        public string NetworkStateName => nameof(ClickObjective);
+
+        private NetworkIdentity _networkIdentity;
         private TextMesh _clickCountText;
 
         private int _clickCount = 0;
@@ -19,12 +22,29 @@ namespace BananaParty.WebSocketRelay.Samples
 
         private void Awake()
         {
+            _networkIdentity = GetComponent<NetworkIdentity>();
             _clickCountText = GetComponentInChildren<TextMesh>();
         }
 
         private void OnMouseDown()
         {
+            if (!_networkIdentity.NetworkAuthority)
+                _networkIdentity.ClaimAuthority();
+
             ClickCount += 1;
+        }
+
+        public void WriteNetworkState(IStateOutput stateOutput)
+        {
+            stateOutput.WriteInt(nameof(_clickCount), _clickCount);
+        }
+
+        public void ReadNetworkState(IStateInput stateInput)
+        {
+            int clickCount = stateInput.ReadInt(nameof(_clickCount));
+
+            if (!_networkIdentity.NetworkAuthority)
+                ClickCount = clickCount;
         }
     }
 }
