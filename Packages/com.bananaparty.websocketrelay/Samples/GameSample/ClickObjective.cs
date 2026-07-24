9c0d5ee76450698ab9d779b3e626ced4d22fe750
forcepusher <bionitsoup@gmail.com>
2026-07-25 00:50:35 +0700

    Update clickcount

--- Packages/com.bananaparty.websocketrelay/Samples/GameSample/ClickObjective.cs
+++ Packages/com.bananaparty.websocketrelay/Samples/GameSample/ClickObjective.cs
@@ -4,9 +4,27 @@ namespace BananaParty.WebSocketRelay.Samples
 {
     public class ClickObjective : MonoBehaviour
     {
+        private TextMesh _clickCountText;
+
+        private int _clickCount = 0;
+        private int ClickCount
+        {
+            set
+            {
+                _clickCount = value;
+                _clickCountText.text = value.ToString();
+            }
+            get => _clickCount;
+        }
+
+        private void Awake()
+        {
+            _clickCountText = GetComponentInChildren<TextMesh>();
+        }
+
         private void OnMouseDown()
         {
-            Debug.Log("On Mouse Down");
+            ClickCount += 1;
         }
     }
 }
